c3a97d54bcf976a73b1e4fb6f381a208a21d3aa0
lexuslight <kostyaelizarov@mail.ru>
2020-09-06 00:26:11 +0300

    Добавлены либы для тс

---
+++ src/Components/PageWrapper/PageWrapper.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import {BrowserRouter, Route} from 'react-router-dom'
+
+const PageWrapper = () => {
+    return (
+        <a>
+            пукпщь4пщ4ь
+        </a>
+    );
+}
+
+export default PageWrapper;
