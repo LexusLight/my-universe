eb6f1b002e33f1225f81f7eb652ba736aae9b969
Hideo Kojima <kostyaelizarov@mail.ru>
2020-09-09 01:05:33 +0300

    Структурка + импорты

--- src/Components/PageWrapper/PageWrapper.tsx
+++ src/Components/PageWrapper/PageWrapper.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import {BrowserRouter, Route} from 'react-router-dom'
+import style from './PageWrapper.module.css'
+import Profile from "../Profile/Profile";
+import World from "../World/World";
+import Character from "../Character/Character";
 
 const PageWrapper = () => {
     return (
-        <a>
-            пукпщь4пщ4ь
-        </a>
+        <BrowserRouter>
+            <div className={style.hat}>
+                Это Обёртка
+            </div>
+            <br/>
+            <Route path={'/profile'}><Profile/></Route>
+            <Route path={'/world'}><World/></Route>
+            <Route path={'/character'}><Character/></Route>
+        </BrowserRouter>
+
     );
 }
 
