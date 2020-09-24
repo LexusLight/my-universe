c7cbbb0317c83004c1c7f70b7624f6e6604f867d
lexuslight <kostyaelizarov@mail.ru>
2020-09-25 00:00:01 +0300

    Либы

--- api_nodejs/server.js
+++ api_nodejs/server.js
@@ -1,12 +1,19 @@
 const path = require("path");
 const express = require('express');
+// import path from 'path'
+// import express from 'express'
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 // запуск статического файлового сервера,
 // который смотрит на папку public/ (в нашем случае отдает index.html)
+
 app.get('/api', function (req, res) {
+    res.send("Бла бла бла бла бла");
+});
+
+app.get('/json', function (req, res) {
     res.json({
         key: "value"
     });
